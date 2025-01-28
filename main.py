6bbc009d4d19742aa3e932cbc7682fedbe9fa95a
IDGS-803-22001758 <79677@alumnos.utleon.edu.mx>
2025-01-27 23:11:24 -0600

    IntroFlask

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():    
+    return "Hola Mundo!!!"
+
+@app.route('/hola')
+def hola():
+    return "Hola!!!"     
+
+@app.route('/user/<string:user>')     
+def user(user):
+    return f"Hola {user}!!!"
+
+@app.route('/numero/<int:n>')
+def numero(n):
+    return "Numero:{}".format(n) 
+
+@app.route('/user/<string:user>/<int:id>')     
+def username(user,id):
+    return f"Nombre: {user} ID:{id}!!!" 
+
+@app.route('/suma/<float:n1>/<float:n2>')     
+def suma(n1,n2):
+    return "La suma es: {}!!!".format(n1+n2)
+
+@app.route('/default')  
+@app.route('/default/<string:nom>')     
+def func(nom='Viviana'):
+    return "El nombre de Nom es: "+nom              
+
+if __name__ == '__main__':
+    app.run(debug=True,port=3000)    
