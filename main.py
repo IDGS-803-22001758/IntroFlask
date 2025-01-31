9a23adc35b2646ff964b56bf02536dd2a8fce4be
IDGS-803-22001758 <79677@alumnos.utleon.edu.mx>
2025-01-30 23:47:29 -0600

    formularios

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():    
-    return "Hola Mundo!!!"
+    grupo="IDGS803"
+    lista=["Juan","Pedro","Mario"]
+    return render_template("index.html", grupo=grupo, lista=lista)
+
+@app.route('/OperasBas')
+def operas():
+    return render_template("OperasBas.html", resulta=None)
+
+@app.route('/resultado', methods=["GET","POST"])
+def resultado():    
+    if request.method == "POST":
+        num1=request.form.get("n1")
+        num2=request.form.get("n2")
+        resulta= int(num1)+int(num2)
+        return render_template("OperasBas.html", resulta=resulta)
+
+@app.route('/ejemplo1')
+def ejemplo1():
+    return render_template("ejemplo1.html")
+
+@app.route('/ejemplo2')
+def ejemplo2():
+    return render_template("ejemplo2.html")    
 
 @app.route('/hola')
 def hola():
@@ -29,7 +51,15 @@ def suma(n1,n2):
 @app.route('/default')  
 @app.route('/default/<string:nom>')     
 def func(nom='Viviana'):
-    return "El nombre de Nom es: "+nom              
+    return "El nombre de Nom es: "+nom     
+
+@app.route("/form1")
+def form1():
+    return '''
+        <form><label>Nombre:</label>
+        <input type="text" name="nombre" placeolder="Nombre">
+        </form>    
+    '''         
 
 if __name__ == '__main__':
     app.run(debug=True,port=3000)    
